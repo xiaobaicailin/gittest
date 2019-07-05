5bc93e68336b8ca2d6c9bce3c9e35d532c9ac550
lzh <2506366099@qq.com>
2019-07-05 11:58:03 +0800

    项目初始化完成......2

--- src/main/java/tees.java
+++ src/main/java/tees.java
@@ -1,5 +1,5 @@
 public class tees {
     public static void main(String[] args) {
-        System.out.println("ssssssssssssssssss55555555");
+        System.out.println("ssssssssssssssssss55555555eeee");
     }
 }
